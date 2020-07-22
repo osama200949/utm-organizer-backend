58028b330e3eb0a984159c4f3d04a910eade1204
osama200949 <osama200949>
2020-07-22 22:31:59 +0800

    Changes in meetings

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,11 @@ const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 const todosRouter = require("./api/controllers/todos_controller");
+const meetingsRouter = require("./api/controllers/meetings_controller");
 
 app.use(express.json());
 app.use("/v1/todos", todosRouter);
+app.use("/v1/meetings", meetingsRouter);
 
 exports.api = functions.https.onRequest(app);
 
